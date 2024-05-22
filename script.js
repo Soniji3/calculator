fd3299281bada1d353491a1fe5ef02f44b0cc43d
Lakshya soni <89504566+Soniji3@users.noreply.github.com>
2024-05-22 15:17:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,47 +3,34 @@ const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
-// Helper function to generate a random color for text 
-// const getRandomColor = () => {
-//   const letters = "0123456789ABCDEF";
-//   let color = "#";
-//   for (let i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// };
-
-//Define function to calculate based on button clicked.
+// Define function to calculate based on button clicked.
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    //If output has '%', replace with '/100' before evaluating.
+    // If output has '%', replace with '/100' before evaluating.
     output = eval(output.replace("%", "/100"));
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
-    //If DEL button is clicked, remove the last character from the output.
+    // If DEL button is clicked, remove the last character from the output.
     output = output.toString().slice(0, -1);
   } else {
-    //If output is empty and button is specialChars then return
+    // If output is empty and button is specialChars then return
     if (output === "" && specialChars.includes(btnValue)) return;
 
+    // If the last character is a special character and the new character is also a special character, replace the last character
+    if (specialChars.includes(output[output.length - 1]) && specialChars.includes(btnValue)) {
+      output = output.slice(0, -1) + btnValue;
+    } else {
+      output += btnValue;
+    }
   }
 
-if(specialChars.includes(output[output.length-1]) && specialChars.includes(btnValue)){
-
-   output=output.slice(0,-1) + btnValue;
-}else
-{
-  output+=btnValue;
-}
-
   display.value = output;
+};
 
- };
-
-//Add event listener to buttons, call calculate() on click.
+// Add event listener to buttons, call calculate() on click.
 buttons.forEach((button) => {
-  //Button click listener calls calculate() with dataset value as argument.
+  // Button click listener calls calculate() with dataset value as argument.
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
